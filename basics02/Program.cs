c080983ff71fd3e9d678409d6fcb7725711d7a72
menna <mennaayman200004@gmail.com>
2026-09-09 16:59:55 +0300

    Declare two separate string variables

--- basics02/Program.cs
+++ basics02/Program.cs
@@ -11,6 +11,14 @@
             Console.WriteLine($"Upper title: {upperTitle}");
             #endregion
 
+            #region Declare two separate string variables, both set to the literal "Clean Code". Use ReferenceEquals() to check if they point to the same object in memory.
+            string string1 = "Clean Code";
+            string string2 = "Clean Code";
+            bool sameRef = object.ReferenceEquals(string1, string2);
+            Console.WriteLine($"Are the strings the same reference? {sameRef}");
+            #endregion
+
+
         }
     }
 }
