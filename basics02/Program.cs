834ddc54b16b1d83e31916bc4764001242c17977
menna <mennaayman200004@gmail.com>
2026-09-09 17:04:36 +0300

    switch

--- basics02/Program.cs
+++ basics02/Program.cs
@@ -79,6 +79,23 @@ namespace basics02
             #endregion
 
 
+            #region Given string title = "Refactoring";, write a switch statement that prints "Great choice!" if the title is "Clean Code", "Nice pick!" if it's "Refactoring", and "Never heard of it" for anything else.
+            string title3 = "Refactoring";
+            switch (title3)
+            {
+                case "Clean Code":
+                    Console.WriteLine("Great choice!");
+                    break;
+                case "Refactoring":
+                    Console.WriteLine("Nice pick!");
+                    break;
+                default:
+                    Console.WriteLine("Never heard of it");
+                    break;
+            }
+            #endregion
+
+
 
 
 
